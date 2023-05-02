cc8e7d189410842d3e569a13bb8fe9ec66d64acf
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2023-05-02 15:51:19 -0300

    erro de contas

---
+++ AppBancoDigital/Exceptions/AccountException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBancoDigital.Exceptions
+{
+    [Serializable]
+    public class AccountException : Exception
+    {
+        public AccountException() { }
+        public int Code { get; set; }
+
+        public AccountException(string message, int number)
+            : base(String.Format("Erro de conta: {0}", message))
+        {
+            Code = number;
+        }
+    }
+}
