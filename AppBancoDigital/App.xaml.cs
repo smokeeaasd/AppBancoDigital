eedd18012279ec1ec6b3a5b976666db15b9eaa98
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2023-05-27 20:45:51 -0300

    Adicionando sistemas de notificações ao vivo

--- AppBancoDigital/App.xaml.cs
+++ AppBancoDigital/App.xaml.cs
@@ -14,50 +14,76 @@ namespace AppBancoDigital
 {
     public partial class App : Application
     {
-        public event EventHandler OnTransacaoRecebida;
+		private delegate void TransacaoDelegate(Correntista remetente, Correntista destinatario, TransacaoCompleta transacao);
+        private event TransacaoDelegate OnTransacaoRecebida;
         public App()
         {
 			Device.SetFlags(new string[] { "AppTheme_Experimental" });
             Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
 
             InitializeComponent();
+
 			if (App.Current.Properties.ContainsKey("id_correntista"))
 			{
                 int id_correntista = int.Parse(App.Current.Properties["id_correntista"].ToString());
+
+				OnTransacaoRecebida += EnviarNotificacaoTransacao;
+				ListenForTransacoes(id_correntista);
 
             	MainPage = new NavigationPage(new Views.Home()
 				{
 					BindingContext = App.Current.Properties["id_correntista"] as int?
 				});
-			} else {
+			}
+			else
+			{
 				MainPage = new NavigationPage(new Views.Inicial());
 			}
         }
 
-        private async Task<List<Conta>> GetContas(int id_correntista)
-        {
-            List<Conta> contas = await DataServiceConta.GetContasByCorrentista(id_correntista);
+		private async void ListenForTransacoes(int id_correntista)
+		{
+			List<Conta> contas = await DataServiceConta.GetContasByCorrentista(id_correntista);
 
-            return contas;
-        }
+			if (contas != null)
+			{
+				foreach (Conta c in contas)
+				{
+					await ListenForTransacoesByConta(c.Id);
+				}
+			}
+		}
 
-        private async void LogTransacoes(int id_correntista)
-        {
-            List<Conta> contas = await GetContas(id_correntista);
+		private List<int> recebidas = new List<int>();
+		private async Task ListenForTransacoesByConta(int id_conta)
+		{
+			await Task.Delay(500);
 
+			TransacaoCompleta transacao = await DataServiceTransacao.GetUltimaCompletaByDestinatario(id_conta);
 
-            foreach (Conta c in contas)
-            {
-                Transacao tc = await DataServiceTransacao.GetUltimaByDestinatario(c.Id);
+			if (transacao != null && !recebidas.Contains(transacao.Id))
+			{
+				recebidas.Add(transacao.Id);
+				Correntista remetente = await DataServiceCorrentista.GetCorrentistaByID(transacao.Remetente.Id_Correntista);
+				Correntista destinatario = await DataServiceCorrentista.GetCorrentistaByID(transacao.Destinatario.Id_Correntista);
 
-                AppServices.SendLogRequest("BancoDigital", JsonConvert.SerializeObject(tc));
-                AppServices.SendLogRequest("BancoDigital", JsonConvert.SerializeObject(tc));
+				OnTransacaoRecebida?.Invoke(remetente, destinatario, transacao);
+			}
+			await ListenForTransacoesByConta(id_conta);
+		}
 
-                await Task.Delay(1000);
-            }
+		private void EnviarNotificacaoTransacao(Correntista correntistaRemetente, Correntista correntistaDestinatario, TransacaoCompleta transacao)
+		{
+			NotificationData data = new NotificationData(
+				"nova_transacao",
+				"Transação",
+				$"{correntistaRemetente.Nome} TE ENVIOU {transacao.ValorFormatado} VIA {transacao.Remetente.TipoFormatado}".ToUpperInvariant(),
+				"channel_transacoes",
+				"Notificação enviada quando o usuário recebe uma transação."
+			);
 
-            LogTransacoes(id_correntista);
-        }
+			AppServices.SendNotificationRequest(data);
+		}
 
         protected override void OnStart()
         {
@@ -72,3 +98,43 @@ namespace AppBancoDigital
         }
     }
 }
+
+
+/**
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using AppBancoDigital.Services;
+using AppBancoDigital.Models;
+
+namespace BancoAPITest
+{
+	internal class Program
+	{
+		private static int IdCorrentista = 11;
+		private delegate void TransacaoData(Correntista remetente, Correntista destinatario, TransacaoCompleta transacao);
+		private static event TransacaoData OnTransacaoRecebida;
+
+		private async static Task Main(string[] args)
+		{
+			OnTransacaoRecebida += RegistrarTransacao;
+			await ListenForTransacoes(IdCorrentista);
+		}
+
+		private static async Task ListenForTransacoes(int id_correntista)
+		{
+			List<Conta> contas = await DataServiceConta.GetContasByCorrentista(id_correntista);
+
+			if (contas != null)
+			{
+				foreach (Conta c in contas)
+				{
+					await ListenForTransacoesByConta(c.Id);
+				}
+			}
+		}
+	}
+}
+*/
